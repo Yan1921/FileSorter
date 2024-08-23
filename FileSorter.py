5b6cac7e7ffda18be1a41079de8408737ba1e3ad
Yan <yan2024@outlook.de>
2024-08-23 22:44:31 +0200

    Removed empty variables and added created_folder counter

--- FileSorter.py
+++ FileSorter.py
@@ -4,37 +4,40 @@ import os as myOS
 unsorted_file_directory = str(r"c:\Users\Yan\Documents\codingExercises\FileSorter-1\Testfolder")
 #Path where sorted folders will go
 sorted_directory = str(r"c:\Users\Yan\Documents\codingExercises\FileSorter-1\Testfolder\sortedFolders")
+counter_folders_created = 0
 counter_moved_files = 0
-
-def createfoldersforgivenextensions(origin_directory, destination_directory):
-    extensions = set(getextensions(origin_directory))
-    for extension in extensions:
-        newfolder = str(destination_directory + r"\\" + extension)
-        if not myOS.path.exists(newfolder):
-            myOS.mkdir(newfolder)
-            print(extension + "  <-- Folder successfully created!")
-        else:
-            print(extension + "  <-- Folder already exists!")
-
 
 def getextensions(directory):
      extensions = []
      for file in myOS.listdir(directory):
-        filename, fileextension = myOS.path.splitext(file)
-        if fileextension != "":
-            extensions.append(fileextension)
+        name_and_extension = myOS.path.splitext(file)
+        if name_and_extension[1] != "":
+            extensions.append(name_and_extension[1])
      return(extensions)
 
-def sortfilestofolders(origin_directory, destination_directory):
+def create_folders_for_given_extensions(origin_directory, destination_directory):
+    global counter_folders_created
+    extensions = set(getextensions(origin_directory))
+    for extension in extensions:
+        newfolder = str(destination_directory + "\\" + extension)
+        if not myOS.path.exists(newfolder):
+            myOS.mkdir(newfolder)
+            print(extension + "  <-- Folder successfully created!")
+            counter_folders_created = counter_folders_created + 1
+        else:
+            print(extension + "  <-- Folder already exists!")
+
+def sort_files_to_folders(origin_directory, destination_directory):
     global counter_moved_files
-    createfoldersforgivenextensions(origin_directory, destination_directory)
+    create_folders_for_given_extensions(origin_directory, destination_directory)
     for file in myOS.listdir(origin_directory):
-        filename, fileextension = myOS.path.splitext(file)
-        if fileextension != "":
+        name_and_extension = myOS.path.splitext(file)
+        if name_and_extension[1] != "":
             print("Origin: " + origin_directory + "\\" + file)
-            print("Destination: " + destination_directory + "\\" + fileextension + "\\" + file)
-            myOS.rename(origin_directory + "\\" + file, destination_directory + "\\" + fileextension + "\\" + file)
+            print("Destination: " + destination_directory + "\\" + name_and_extension[1] + "\\" + file)
+            myOS.rename(origin_directory + "\\" + file, destination_directory + "\\" + name_and_extension[1] + "\\" + file)
             counter_moved_files = counter_moved_files + 1
 
-sortfilestofolders(unsorted_file_directory, sorted_directory)
+sort_files_to_folders(unsorted_file_directory, sorted_directory)
+print("Folders created: " + str(counter_folders_created))
 print("Moved files: " + str(counter_moved_files))
