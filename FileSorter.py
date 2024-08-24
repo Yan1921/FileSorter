fb4cdad34d3e0ffc4acf1bd74b412d1afd167a4b
Yan <yan2024@outlook.de>
2024-08-24 14:54:20 +0200

    Some fixes; Removed not_created_folders counter bcs not necessary

--- FileSorter.py
+++ FileSorter.py
@@ -4,7 +4,6 @@ unsorted_file_directory = str(myOS.getcwd())        	     #Directory with stuff 
 sorted_directory = str(f"{myOS.getcwd()}\sortedFiles")     #Path where sorted folders will go
 
 counter_folders_created = 0
-counter_folders_not_created = 0
 counter_moved_files = 0
 counter_not_moved_files = 0
 ignored_extensions = [".py", ".txt", "", ".md"]
@@ -19,23 +18,17 @@ def getextensions(directory):
 
 def create_folders_for_given_extensions(origin_directory, destination_directory):
     global counter_folders_created
-    global counter_folders_not_created
     extensions = set(getextensions(origin_directory))
     if not myOS.path.exists(destination_directory):
         myOS.mkdir(destination_directory)
         counter_folders_created = counter_folders_created + 1
         print("sortedFiles folder created!")
-    else:
-        counter_folders_not_created = counter_folders_not_created + 1
     for extension in extensions:
         newfolder = str(f"{destination_directory}\{extension}")
         if not myOS.path.exists(newfolder) and extension not in ignored_extensions:
             myOS.mkdir(newfolder)
             print(f"{extension} <-- Folder successfully created!")
             counter_folders_created = counter_folders_created + 1
-        else:
-            print(f"{extension} <-- Folder already exists!")
-            counter_folders_not_created = counter_folders_not_created + 1
 
 def sort_files_to_folders(origin_directory, destination_directory):
     global counter_moved_files
@@ -55,5 +48,4 @@ def sort_files_to_folders(origin_directory, destination_directory):
 sort_files_to_folders(unsorted_file_directory, sorted_directory)
 print(f"Folders created: {str(counter_folders_created)}")
 print(f"Moved files: {str(counter_moved_files)}")
-print(f"Folders not created: {str(counter_folders_not_created)}")
 print(f"Not moved files: {str(counter_not_moved_files)}")
